e103f190d71a4efdf881a03b17b5a51e16b90b72
charles-m-knox <8022749+charles-m-knox@users.noreply.github.com>
2015-12-09 16:26:26 -0700

    master

--- metropolis_algorithm.py
+++ metropolis_algorithm.py
@@ -25,78 +25,9 @@ from scipy.optimize import curve_fit
 def log(message):
     print message
 
-def gen_particle_dist(temp, num_particles, num_trials, energy_max):
+def gen_mcs_dist(temp, num_particles, num_trials, energy_max):
     beta = float(1/float(temp)) #Thermodynamic Beta = 1/kT
     
-    energy_progression = [] #For each trial, we will record the total energy
-
-    def get_highest_energy(particle_collection):
-        highest_energy_val = 0
-        try:
-            for particle in particle_collection:
-                if particle['E'] > highest_energy_val:
-                    highest_energy_val = particle['E']
-            return int(highest_energy_val)
-        except Exception as e:
-            log("Failed to get highest energy: " + str(e))
-            return int(energy_max)
-
-    def get_random_particle_index(num_particles):
-        try:
-            return random.randint(0,num_particles)
-        except Exception as e:
-            log('Failed to generate random particle index given ' 
-                + str(num_particles) + ' as input:' + str(e))
-            return 0
-
-    def alter_energy_of_particle(particle, energy_change):
-        try:
-            particle['E'] += int(energy_change)
-            particle['P'] = math.exp(-(float(1)/float(temp))*float(
-                                particle['E']))
-        except Exception as e:
-            log('Failed to alter energy of particle:' + str(e))
-
-    def alter_random_particle(particle_collection, energy_change):
-        try:
-            random_particle_index = get_random_particle_index(
-                                        len(particle_collection)-1) 
-            alter_energy_of_particle(
-                particle_collection[random_particle_index],
-                energy_change)
-        except Exception as e:
-            log('Failed to alter random particle: ' + str(e))
-
-    def generate_particle(energy_max):
-        return_dict = {'P': 0, 'E': int(1)}
-        try:
-            return_dict['E'] = int(random.randint(1, energy_max))
-            return_dict['P'] = math.exp(
-                                    -(1/temp)
-                                    * float(return_dict['E']))
-        except Exception as e:
-            log('Failed to generate particle: ' + str(e))
-        return return_dict
-
-    def generate_particle_list(num_particles, energy_max):
-        return_list = []
-        try:
-            for x in range(0,num_particles):
-                particle = generate_particle(energy_max)
-                return_list.append(particle)
-        except Exception as e:
-            log('Failed to generate particle list:' + str(e))
-        return return_list
-
-    def get_total_energy(particle_collection):
-        total_energy = 0
-        try:
-            for particle in particle_collection:
-                total_energy += particle['E']
-        except Exception as e:
-            log('Failed to get total energy of macrostate:' + str(e))
-        return total_energy
-
     def get_random_energy_change_value():
         try:
             if random.randint(0,1) == 1:
@@ -107,46 +38,45 @@ def gen_particle_dist(temp, num_particles, num_trials, energy_max):
             log("Failed to get random energy change value:" + str(e))
             return 0
 
-    #Step 1: Initialize an dictionary list of N length, with each item
-    # containing { P_i, E_i } - these are particles.
-    particle_collection = generate_particle_list(num_particles, energy_max)
-    
+    particle_collection = []
+    for i in range(0, num_particles):
+        particle_collection.append(energy_max/10)
+
     #Step 2, N times: Change energy of random particle, compute total 
     # energy of system, then accept or do not accept.
+    steps = []
+    energy_steps = []
     for i in range(0, num_trials):
-        particle_collection_unaltered = particle_collection
-        unaltered_macrostate_energy = get_total_energy(particle_collection)
+        particle_collection_unaltered = []
+        for particle in particle_collection:
+            particle_collection_unaltered.append(particle)
+        #Calculate total energy of system
+        initial_macrostate_energy = sum(particle_collection)
         microstate_energy_change = get_random_energy_change_value()
-        alter_random_particle(particle_collection, microstate_energy_change)
-        altered_macrostate_energy = get_total_energy(particle_collection)
-        dE = altered_macrostate_energy - unaltered_macrostate_energy
-        # If the energy change is DE<0, accept the change
-        #if dE < 0:
-        #    log("Accepting change")
-        #If the energy change is DE>0, accept the change only if 
-        # r<exp(-b*DE);  where r is a randomly generated number 0<r<1:
+        #Alter the energy of a random particle
+        random_particle_index = random.randint(0, len(particle_collection) - 1)
+        if particle_collection[random_particle_index] > 1:
+            particle_collection[random_particle_index] += microstate_energy_change
+        altered_macrostate_energy = sum(particle_collection)
+        dE = altered_macrostate_energy - initial_macrostate_energy
         if dE > 0:
             random_num_r = random.random()
             energy_condition_value = math.exp(-(beta)*dE)
             if random_num_r > energy_condition_value:
                 particle_collection = particle_collection_unaltered
-        elif dE == 0:
-            log("Got zero for energy change. Doing nothing.")
-        total_macrostate_energy = get_total_energy(particle_collection)
-        energy_progression.append(total_macrostate_energy)
+        total_macrostate_energy = sum(particle_collection)
+        steps.append(i)
+        energy_steps.append(total_macrostate_energy)
 
-    energies_list = []
     probabilities_list = []
-    partition_function = 0
+    partition_fct = float(0)
+    #First get the partition function by adding all particles' exponential fcts
     for particle in particle_collection:
-        energies_list.append(particle['E'])
-        partition_function += particle['P']
+        #print str(particle) + " " + str(beta) + " " + str(partition_fct)
+        partition_fct += math.exp(-beta * particle)
     for particle in particle_collection:
-        original_particle_P = particle['P']
-        particle['P'] = float(1)/float(partition_function)*original_particle_P
-        probabilities_list.append(particle['P'])
-
-    return energies_list, probabilities_list
+        probabilities_list.append(1/partition_fct * math.exp(-beta * particle))
+    return particle_collection, probabilities_list, steps, energy_steps
 
 def gen_complete_distributions(num_particles, 
                 plots_temperatures, 
@@ -154,6 +84,7 @@ def gen_complete_distributions(num_particles,
                 num_trials,
                 run_number):
     complete_data_set = []
+    monte_carlo_steps_array = []
     fig = plt.figure(figsize=(8.5,11))
     plot_index = 0
     for z in range(0,10):
@@ -165,11 +96,12 @@ def gen_complete_distributions(num_particles,
             plt.ylabel(r"$\mathrm{Probability}$", fontsize=12)
         if plot_index >= 9:
             plt.xlabel(r"$\mathrm{Energy}$", fontsize=12)
-        x_vals, y_vals = gen_particle_dist(
+        x_vals, y_vals, x_steps, y_steps = gen_mcs_dist(
                 plots_temperatures[z], 
                 num_particles, 
                 num_trials, 
                 maximum_energy_all_plots)
+        monte_carlo_steps_array.append([x_steps,y_steps])
         #plt.xticks([0,max(x_vals)])
         #plt.yticks([0,max(0.11)])
         plt.tick_params(axis='y', which='major', labelsize=5)
@@ -184,12 +116,41 @@ def gen_complete_distributions(num_particles,
             'T': plots_temperatures[z],
             'N': num_particles,
             'E_avg': 0})
-    fig.suptitle(r"$\mathrm{Trials}=" + str(num_trials) + r", n=" +
-                    str(num_particles) + r"\mathrm{\,Particles}$", fontsize=18)
+    fig.suptitle(r"$\mathrm{Trials}=" + str(num_trials) + r",\,n=" +
+                    str(num_particles) + r"\mathrm{\,Particles\,-\,Run\," + str(run_number) + r"}$", fontsize=18)
     plt.savefig("distribution_" + str(num_particles) + "_particles" + 
                     str(run_number) + ".png")
     plt.savefig("distribution_" + str(num_particles) + "_particles" + 
                     str(run_number) + ".svg")
+    #Render an entirely different set of plots for Monte Carlo Steps
+    fig = plt.figure(figsize=(8.5,11))
+    plot_index = 0
+    for z in range(0,10):
+        plot_index += 1
+        plt.subplot(5, 2, plot_index)
+        plt.title(r"$T=" + str(plots_temperatures[z])+r"$", fontsize=12)
+        #Decorative purposes; only show y-axis label on left, x label on bottom
+        if plot_index % 2 != 0:
+            plt.ylabel(r"$\mathrm{Total\,Energy}$", fontsize=12)
+        if plot_index >= 9:
+            plt.xlabel(r"$\mathrm{Monte\,Carlo\,Steps}$", fontsize=12)
+        plt.tick_params(axis='y', which='major', labelsize=5)
+        plt.tick_params(axis='y', which='minor', labelsize=5)
+        plt.tick_params(axis='x', which='major', labelsize=5)
+        plt.tick_params(axis='x', which='minor', labelsize=5)
+        plt.grid(axis="both", alpha=0.10, linestyle="-")
+        plt.scatter(monte_carlo_steps_array[z][0], 
+                    monte_carlo_steps_array[z][1], 
+                    s=1, 
+                    c="r")
+    fig.suptitle(r"$\mathrm{Trials}=" + str(num_trials) + r", n=" +
+                    str(num_particles) + r"\mathrm{\,Particles}$", fontsize=18)
+    plt.savefig("mcs_" + str(num_particles) + "_particles" + 
+                    str(run_number) + ".png")
+    #plt.savefig("mcs_" + str(num_particles) + "_particles" + 
+    #                str(run_number) + ".svg")
+
+
     #Now compute the average energy <E> for each data set
     E_averages = []
     for data_set in complete_data_set:
@@ -208,10 +169,10 @@ def gen_complete_distributions(num_particles,
 
 def main(args_dict):
     num_particles = int(args_dict['num_particles'])
-    plots_temperatures = [10,50,100,500,1000,3000,6000,10000,15000,100000]
+    plots_temperatures = [1,5,10,50,100,500,1000,5000,10000,100000]
     maximum_energy_all_plots = int(args_dict['energy_max'])
     num_trials = int(args_dict['num_trials'])
-    runs = 5
+    runs = 1
     average_energies = []
     for xx in range(0, runs):
         complete_data_set, E_averages = gen_complete_distributions(
@@ -223,7 +184,14 @@ def main(args_dict):
         average_energies.append(E_averages)
     fig = plt.figure(figsize=(8.5,11))
     for average_energy_list in average_energies:
-        plt.semilogx(plots_temperatures, average_energy_list)
+        plt.loglog(plots_temperatures, average_energy_list)
+    #calculate analytical <E> values
+    analytical_energy_averages = []
+    for temp in plots_temperatures:
+        exp_val = math.exp(-(float(1)/float(temp)))
+        analytical_energy_value = exp_val / (1 - exp_val)
+        analytical_energy_averages.append(analytical_energy_value)
+    plt.loglog(plots_temperatures, analytical_energy_averages)
     fig.suptitle(r"$\langle E\left(T\right)\rangle \mathrm{\,values:\,}n=" + 
                     str(num_particles) + r"\mathrm{,\,Trials}=" + 
                     str(num_trials) + "$", fontsize=18)
@@ -232,7 +200,7 @@ def main(args_dict):
     plt.xlabel(r"$\mathrm{Temperature\,(K)}$", fontsize=16)
     plt.tick_params(axis='both', which='major', labelsize=6)
     plt.grid(axis="both", which='major', alpha=0.25, linestyle="-")
-    plt.grid(axis="x", which='minor', alpha=0.10, linestyle="-")
+    plt.grid(axis="both", which='minor', alpha=0.10, linestyle="-")
     plt.savefig("e_averages_" + str(num_particles) + "_particles.svg")
     plt.savefig("e_averages_" + str(num_particles) + "_particles.png")
 
